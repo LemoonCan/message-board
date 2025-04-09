45b12d3c0ed28ec6277317d63a6fb0ad143dc57d
Lemoon Can <lemoon.can@gmail.com>
2025-04-09 23:27:20 +0800

    refactor: 💡 新增点赞记录表

--- message-board-backend/src/main/resources/schema.sql
+++ message-board-backend/src/main/resources/schema.sql
@@ -6,18 +6,19 @@ CREATE TABLE IF NOT EXISTS customer
     password      VARCHAR(255) NOT NULL COMMENT '密码',
     email         VARCHAR(255) NOT NULL UNIQUE COMMENT '邮箱',
     last_login_at TIMESTAMP COMMENT '最后登录时间',
-    created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间'
+    created_at    TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    updated_at    TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 );
 
 -- 留言表
 CREATE TABLE IF NOT EXISTS message
 (
-    id           BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
-    content      CLOB      NOT NULL comment '留言内容',
-    customer_id  TINYINT   NOT NULL COMMENT '用户ID',
-    parent_id    BIGINT COMMENT '父留言ID',
-    created_at   TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    updated_at   TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
+    id          BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '主键',
+    content     CLOB      NOT NULL comment '留言内容',
+    customer_id TINYINT   NOT NULL COMMENT '用户ID',
+    parent_id   BIGINT COMMENT '父留言ID',
+    created_at  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    updated_at  TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间'
 );
 
 -- 点赞记录表
